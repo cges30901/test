48a56e8e932f3e7f5d112fc0f3038f890d968754
Hsiu-Ming Chang <cges30901@gmail.com>
2020-02-15 11:15:50 +0800

    update id code

--- hash/main.cpp
+++ hash/main.cpp
@@ -5,7 +5,16 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QWebEngineView view;
-    view.load(QUrl::fromLocalFile(a.applicationDirPath()+"/test.html"));
+
+    //open remote html with id is working
+    //view.load(QUrl("https://cges30901.github.io/test/hash/test#p3"));
+
+    //open local file is working
+    //view.load(QUrl::fromLocalFile(a.applicationDirPath()+"/test.html"));
+
+    //open local file with id is not working
+    view.load(QUrl::fromLocalFile(a.applicationDirPath()+"/test.html#p3"));
+
     view.show();
     return a.exec();
 }
