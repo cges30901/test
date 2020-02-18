a3f4a9a341e73e3334c0052e6733bf2dea6037ef
Hsiu-Ming Chang <cges30901@gmail.com>
2020-02-18 17:18:31 +0800

    Scroll in multicolumn

--- multicolumn/mainwindow.py
+++ multicolumn/mainwindow.py
@@ -1,16 +1,44 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
-from PyQt5.QtCore import QUrl, pyqtSlot
+from PyQt5.QtCore import QUrl, pyqtSlot, QEvent
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.view = QWebEngineView(self)
-        self.view.load(QUrl("https://cges30901.github.io/test/vert2"))
+        self.view.load(QUrl.fromLocalFile("/home/manjaro/project/epub/ol.epub_FILES/OEBPS/Text/text02.xhtml"))
         self.setCentralWidget(self.view)
         self.view.loadFinished.connect(self.paginate)
+        self.view.focusProxy().installEventFilter(self)
+        self.pos = 0
+
+    def eventFilter(self, source, e):
+        if source == self.view.focusProxy():
+            #pos_js = self.view.page().scrollPosition().x() - self.view.page().contentsSize().width() + self.view.width()
+            if e.type() == QEvent.Wheel:
+                col=self.view.page().contentsSize().height()//self.view.height()
+                height=self.view.page().contentsSize().height()/col
+                #print(height)
+                if e.angleDelta().y() > 0:
+                    self.pos -= height
+                    if self.pos<0:
+                        self.pos=0
+                    self.view.page().runJavaScript("window.scrollTo({0}, {1});"
+                        .format(self.view.page().scrollPosition().x() , self.pos))
+                else:
+                    self.pos += height
+                    if self.pos>self.view.page().contentsSize().height()-self.view.height():
+                        self.pos=self.view.page().contentsSize().height()-self.view.height()
+                    self.view.page().runJavaScript("window.scrollTo({0}, {1});"
+                        .format(self.view.page().scrollPosition().x() , self.pos))
+                print(self.pos, self.view.page().scrollPosition().y(), self.view.page().contentsSize().height())
+                return True
+            else:
+                return False
+        return False
+                
 
     @pyqtSlot(bool)
     def paginate(self):
